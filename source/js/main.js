389076c2c935436df5686d4a02e5af078fe3d5d9
_ngmanh <manhnguyen@yola.vn>
2017-05-30 17:41:09 +0700

    :lipstick: update UI && styles

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,16 @@
 (function ($) {
   $('.owl-carousel').owlCarousel({
     center: true,
+    loop: true,
+    margin: 16,
+    items: 2,
+    responsive: {
+      1024: {
+        items: 6
+      },
+      640: {
+        items: 4,
+      }
+    }
   });
-  // console.log('change');
 })(jQuery)
