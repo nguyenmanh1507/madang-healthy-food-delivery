ac27483c3494842cf4ff40052480222e88b1f331
nguyenmanh1507 <nguyenmanh1507@gmail.com>
2017-07-05 17:32:15 +0700

    update

--- source/js/main.js
+++ source/js/main.js
@@ -1,3 +1,5 @@
+/* global jQuery */
+
 (function ($) {
   $('#featuredCarousel').slick({
     centerMode: true,
@@ -8,39 +10,52 @@
       {
         breakpoint: 1640,
         settings: {
-          slidesToShow: 6,
-        },
+          slidesToShow: 6
+        }
       },
       {
         breakpoint: 1390,
         settings: {
-          slidesToShow: 5,
-        },
+          slidesToShow: 5
+        }
       },
       {
         breakpoint: 1120,
         settings: {
-          slidesToShow: 4,
-        },
+          slidesToShow: 4
+        }
       },
       {
         breakpoint: 900,
         settings: {
-          slidesToShow: 3,
-        },
+          slidesToShow: 3
+        }
       },
       {
         breakpoint: 660,
         settings: {
-          slidesToShow: 2,
-        },
+          slidesToShow: 2
+        }
       },
       {
         breakpoint: 480,
         settings: {
-          slidesToShow: 1,
-        },
-      },
-    ],
-  });
+          slidesToShow: 1
+        }
+      }
+    ]
+  })
+
+  $('#testimonialCarousel').slick({
+    dots: true,
+    arrows: false,
+    fade: true
+  })
+
+  $('#instagramCarousel').slick({
+    arrows: false,
+    centerMode: true,
+    slidesToShow: 7,
+    focusOnSelect: true
+  })
 })(jQuery)
